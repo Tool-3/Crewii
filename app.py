db7eb06fbbfeb92d12946c86355e82ba5239c076
Tool-3 <tool.3074@gmail.com>
2025-07-01 18:10:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from scraper import scrape_linkedin_profile
+from linkedin_scraper import scrape_linkedin_profile
 
 st.set_page_config(page_title="LinkedIn Profile Scraper", layout="wide")
 
